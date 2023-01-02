93f72ba3bf9ef42002f82f2cf492820f2bb42544
second <a@b.ccccc>
2023-01-02 10:52:33 +0000

    added more summary data to summary report

--- process/F_evaluate_model/notebooks/create_model_summary_report.py
+++ process/F_evaluate_model/notebooks/create_model_summary_report.py
@@ -14,13 +14,10 @@ from bs4 import BeautifulSoup
 
 def include_in_html_report(type, section_header=None, section_figure=None, section_content=None, section_content_list=None):
 
-    # writePath_html = r'model_results/%s (html).html' % key
-    # writePath_md = r'model_results/%s (md).md' % key
     results_root = '../../F_evaluate_model'
     writePath_html = f'{results_root}/html/{key}.html'.replace(" ", "_").replace("(", "_").replace(")", "_")
     writePath_md = f'{results_root}/markdown/{key}.md'
 
-#isinstance(ini_list2, list)
     if not section_content_list:
         section_content_list = [section_content]
 
@@ -63,33 +60,15 @@ def include_in_html_report(type, section_header=None, section_figure=None, secti
                 f2.write('\n')
         elif type=='json':
 
-            # html_content_parsed = [[cell.text for cell in row("td")]
-            #              for row in BeautifulSoup(content,features="html.parser")("tr")]
-            #
-            # html_content_dictionary = {element[0]:element[1:] for element in html_content_parsed}
-
-            #xxxprint(json.dumps(html_content_dictionary, indent=4))
-
-
-
             with open(writePath_html, 'a') as f1:
                 #f.write(json.dumps(html_content_dictionary, indent=4))
                 soup = BeautifulSoup(section_content, "html.parser")
                 f1.write(str(soup.prettify()))
             with open(writePath_md, 'a') as f2:
-                #f.write(json.dumps(html_content_dictionary, indent=4))
                 soup = BeautifulSoup(section_content, "html.parser")
-                #f2.write(str(soup.prettify()))
-
-
-                # html_content_dictionary = {element[0]:element[1:] for element in html_content_parsed}
-                # f2.write(json.dumps(html_content_dictionary, indent=4))
 
                 import ast
                 loads = ast.literal_eval(section_content)
-                #df = pd.DataFrame.from_dict(loads)
-                #df.drop(['dont'], axis=1, inplace=True)
-                #print(df.to_markdown(index=False,tablefmt='fancy_grid'))
                 for each in loads:
                     f2.write(each + " = " + str(loads[each]) + "\n\n")
 
@@ -129,74 +108,83 @@ include_in_html_report(type="text", section_header=f"Dataset Version: N/A", sect
 ])
 include_in_html_report("header", section_content=f"Results", section_figure=2)
 
-#xxxinclude_in_html_report(type="text", section_header="Summary", section_content=new_model_decision)
-
-#include_in_html_report(type="dataframe",text_single="Tuned Models ranked by performance", content=cv_results_df_sorted)
-
-'''if not using_catboost:
-    include_in_html_report(type='dataframe', section_header='Tuned Models ranked by performance, with parameter details', section_content=cv_results_df_summary)
-
-    include_in_html_report(type='graph', section_header='Best and worst models obtained by tuning', section_figure=worst_and_best_model_fig, section_content="best_and_worst.png")
-
-    include_in_html_report(type='graph', section_header='Best Model: Comparing model predictions to actual property values', section_figure=best_model_fig, section_content='best_model_correlation.png')
-else: #if using_catboost:
-    include_in_html_report(type="text", section_header="Model Specific Notes", section_content_list=["can't display hyperparameter comparison for catboost","can't display model performance graphs for catboost","can't display model performance graphs for catboost"])
-
-
-if model_uses_feature_importances:
-    include_in_html_report("header", section_content=f"Feature Importances", section_figure=2)
-    include_in_html_report(type="text", section_header="Feature Importances", section_content=feature_importances_output)
-    include_in_html_report(type="graph", section_header=f"Feature Importances ({ALGORITHM})", section_figure=feature_importance_fig, section_content='best_model_feature_importances.png')
-'''
-
 include_in_html_report("header", section_content=f"Comparison with other models", section_figure=2)
 
 
 dff = pd.read_json('../../../results/results.json')
 
-#version = VERSION
-
-
 all_models_df = dff[dff.columns].T.sort_values("best score", ascending=False)
-#version_models_df = dff[[c for c in dff.columns if version in c]].T.sort_values("best score", ascending=False)
 
-#version_models_summary = version_models_df[['best score', 'best time', 'Mean Absolute Error Accuracy', 'Mean Squared Error Accuracy', 'R square Accuracy', 'Root Mean Squared Error', 'best run date', 'best method']]
 all_models_summary = all_models_df[['best score', 'best time', 'Mean Absolute Error Accuracy', 'Mean Squared Error Accuracy', 'R square Accuracy', 'Root Mean Squared Error', 'best run date', 'best method']]
 
-#include_in_html_report(type="dataframe", section_header=f"Comparison with version {VERSION} performances", section_content=version_models_summary)
 include_in_html_report(type="dataframe", section_header="Comparison with all model performances", section_content=all_models_summary)
 
 
 include_in_html_report("header", section_content=f"Appendix", section_figure=2)
 
-#include_in_html_report(type="dataframe", section_header="Data Sample", section_content=df.head(5))
 
-'''include_in_html_report(type="json", section_header="Hyperparameter options for Randomized Grid Search", section_content=f"{param_options if not using_catboost else options_block}")
+df_summary_1_vs_2 = all_models_df[
+    ['best score', 'best time', 'silver score', 'silver time', 'best method', 'silver method', 'best is shared']]
+df_summary_1_vs_2
 
-if not using_catboost:
-    include_in_html_report(type="graph", section_header=f"Range of hyperparameter results", section_figure=evolution_of_models_fig,
-        section_content='evolution_of_models_fig.png')
+df_1_vs_2b = all_models_df[
+    ['best score', 'best time', 'silver score', 'silver time', 'best params', 'silver params', 'best is shared','best method','silver method']]
+df_1_vs_2b
 
-include_in_html_report(type="dict", section_header="Environment Variables", section_content=env_vars)
-'''
+d1 = all_models_df.iloc[0]['best params']
+d2 = all_models_df.iloc[0]['silver params']
+d99 = all_models_df.iloc[0]['_params']
+d1['score'] = all_models_df.iloc[0]['best score']
+d2['score'] = all_models_df.iloc[0]['silver score']
+d99['score'] = all_models_df.iloc[0]['_score']
+d1['time'] = all_models_df.iloc[0]['best time']
+d2['time'] = all_models_df.iloc[0]['silver time']
+d99['time'] = all_models_df.iloc[0]['_train time']
+d1['date run'] = all_models_df.iloc[0]['best run date']
+d2['date run'] = all_models_df.iloc[0]['silver run date']
+d99['date run'] = all_models_df.iloc[0]['date']
+d1['method'] = all_models_df.iloc[0]['best method']
+d2['method'] = all_models_df.iloc[0]['silver method']
+d99['method'] = all_models_df.iloc[0]['_method']
 
-def print_and_report(text_single, title):
-    include_in_html_report("text", section_content=title)
-    for each in text_single:
-        print(each)
-        include_in_html_report("text", section_header="", section_content=each)
+res = {}
+for each in d1:
+    res[each] = [d1[each]]
+    if each in d2:
+        if each not in res:
+            d2[each]
+            res[each]
+        res[each].extend([d2[each]])
+    else:
+        res[each].append('not found')
 
-# if not catboost:
-#     print_and_report([
-#         'Best Index:' + str(crossval_runner.best_index_) + '<br>',
-#         'Best Score:' + str(crossval_runner.best_score_) + '<br>',
-#         'Best Params: ' + str(crossval_runner.best_params_) + '<br>'
-#     ], "Best Model Details")
+including_most_recent = True
+if including_most_recent == True:
+    for each in d1:
+        if each in d99:
+            if each not in res:
+                d2[each]
+                res[each]
+            res[each].extend([d99[each]])
+        else:
+            #d1[each] = d99[each]
+            pass
 
 
+run_comparison = pd.DataFrame.from_dict(res).T
+if including_most_recent:
+    run_comparison.columns = ['best model','runner-up model','most recent model']
+else:
+    run_comparison.columns = ['best model','runner-up model']
+    
+model = all_models_df.iloc[0].index
 
-# In[138]:
 
+include_in_html_report(type="dataframe", section_header="Analyse something else", section_content=df_summary_1_vs_2)
+
+include_in_html_report(type="dataframe", section_header="Analyse a 3rd thing", section_content=df_1_vs_2b)
+
+include_in_html_report(type="dataframe", section_header=f"Analysing the hyperparameters for the best performing version (for {model})", section_content=run_comparison)
 
 print('Nearly finished...')
 
